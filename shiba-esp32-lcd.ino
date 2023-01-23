3cfc7284ccabc8a225bf347226614b01991e98cb
Joe <altair.dracolich@gmail.com>
2023-01-22 21:29:24 -0500

    Update shiba-esp32-lcd.ino

--- shiba-esp32-lcd.ino
+++ shiba-esp32-lcd.ino
@@ -14,9 +14,35 @@ const char* password = "YOURPASSWORD"; // Enter your wifi password
 
 double prevPrice = 0;
 
+// Down arrow
+byte customChar1[8] = {
+  B00000,
+  B00100,
+  B01110,
+  B10101,
+  B00100,
+  B00100,
+  B00100,
+  B00000
+};
+
+// Up arrow
+byte customChar2[8] = {
+  B00000,
+  B00100,
+  B00100,
+  B00100,
+  B10101,
+  B01110,
+  B00100,
+  B00000
+};
+
 void setup() {
   lcd.begin();                           // Initialize the LCD connected
   lcd.backlight();                       // Turn on the backlight on LCD.
+  lcd.createChar(0, customChar1);
+  lcd.createChar(1, customChar2);
   WiFi.begin(ssid, password);            // Setup wifi connection
   lcd.setCursor(0, 0);                   // Set cursor to the first character space  (0) of the first line (0)
   lcd.print("Establish WiFi..");         // Print a message to the cursor position
@@ -30,7 +56,7 @@ void setup() {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.println("Connected to: ");
-  lcd.setCursor(0, 1);
+  lcd.setCursor(0, 1);  // Display message once connected
   lcd.print(ssid);
   delay(2500);
   lcd.clear();
@@ -39,34 +65,41 @@ void setup() {
 void loop() {
   double currentPrice = getSHIBPrice();  // Get the data
   lcd.setCursor(0, 0);                   // Set cursor line 1 character 1
-  lcd.print("SHIB: $");                  // Print current price
-  lcd.print(currentPrice, 8);            // Current price at 8 decimal places
+  lcd.print("SHIB $");                  // Print current price
+  lcd.print(currentPrice, 13);            // Current price at 8 decimal places
   if (prevPrice > 0) {                   // Check for price change
     double change = currentPrice - prevPrice;
     lcd.setCursor(0, 1);
     if (change > 0) {
-      lcd.print("Up: $");
-      lcd.print(change, 8);
+      lcd.print((char)1);
+      lcd.print(" $");
+      lcd.print(change, 13);
     } else if (change < 0) {
-      lcd.print("Down: $");
-      lcd.print(change, 8);
+      lcd.print((char)1);
+      lcd.print(" $");
+      lcd.print(change, 13);
     } else {
-      lcd.print("No Change");
+      lcd.setCursor(0, 1);
+      lcd.print("   No  Change   ");
     }
   }
+
   prevPrice = currentPrice;  // Update previous price
-  delay(19 * 60 * 1000);     // Wait for 19 minuets before updating -- change 19 to whatever you want. FYI Coingecko has a limit on free API requests.
+  delay(1 * 60 * 1000);      // Wait for 19 minuets before updating
 }
 
 double getSHIBPrice() {
   HTTPClient http;
+
   // Make API call
-  http.begin("https://api.coingecko.com/api/v3/simple/price?ids=shiba-inu&vs_currencies=usd&precision=9");
+  http.begin("https://api.coingecko.com/api/v3/simple/price?ids=shiba-inu&vs_currencies=usd&precision=14");
   int httpCode = http.GET();
+
   if (httpCode > 0) {
     // Parse JSON response
     DynamicJsonDocument doc(1024);
     deserializeJson(doc, http.getString());
+
     // Get current price
     double currentPrice = doc["shiba-inu"]["usd"];
     return currentPrice;
